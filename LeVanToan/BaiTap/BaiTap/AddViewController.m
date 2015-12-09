dae15f269f6a26589e53cc61b068630443f62503
vantoan2509 <vantoan2509@gmail.com>
2015-12-09 18:12:12 +0700

    9/12/2015

--- LeVanToan/BaiTap/BaiTap/AddViewController.m
+++ LeVanToan/BaiTap/BaiTap/AddViewController.m
@@ -9,7 +9,7 @@
 #import "AddViewController.h"
 #import "CarDAO.h"
 
-@interface AddViewController ()
+@interface AddViewController ()<UIImagePickerControllerDelegate,UIActionSheetDelegate>
 @property(weak, nonatomic) IBOutlet UITextField *model;
 @property(weak, nonatomic) IBOutlet UITextField *carNumber;
 @property(weak, nonatomic) IBOutlet UIImageView *avatar;
@@ -38,6 +38,53 @@
     NSData* dtAvatar = UIImageJPEGRepresentation(self.avatar.image, 1.0);
     [[CarDAO shared] addCar:self.model.text carNumber:self.carNumber.text fuelCapaticy:nil maxSpeed:nil numberOfSeats:nil createDay:date avatar:dtAvatar];
     [self.navigationController popViewControllerAnimated:YES];
+}
+-(IBAction)atChangeAvatar:(id)sender{
+    UIActionSheet *popup = [[UIActionSheet alloc] initWithTitle:@"Select Sharing option:" delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:nil otherButtonTitles:
+                            @"choose picture form your device",
+                            @"take a photo",
+                            @"dowload from internet",
+                            nil];
+    [popup showInView:self.view];
+}
+
+//action sheet selegate
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex{
+    if (buttonIndex == 0) {
+        UIImagePickerController* ipc = [[UIImagePickerController alloc] init];
+        ipc.delegate = self;
+        ipc.allowsEditing = YES;
+        ipc.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        
+        [self presentViewController:ipc animated:YES completion:NULL];
+    }
+    else if (buttonIndex == 1){
+        UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+        picker.delegate = self;
+        picker.allowsEditing = YES;
+        if ([UIImagePickerController isSourceTypeAvailable:
+             UIImagePickerControllerSourceTypeCamera] == YES)
+        {
+            picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        } else {
+            picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        }
+        
+        
+        
+        [self presentViewController:picker animated:YES completion:NULL];
+    }
+}
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
+    self.avatar.image = chosenImage;
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+}
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingImage:(UIImage *)image editingInfo:(NSDictionary *)editingInfo{
+    self.avatar.image = image;
+    [picker dismissViewControllerAnimated:YES completion:NULL];
 }
 
 @end
