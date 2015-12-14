7ab3e1eddfc93f910f823ec08c6d346c320e0264
khoabkdn <mrhoa@MrHoas-Mac.local>
2015-12-13 18:09:43 -0800

    Initial Commit

---
+++ PhamKhanhHoa/HoaBT2/HoaBT2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HoaBT2
+//
+//  Created by MrHoa on 12/13/15.
+//  Copyright (c) 2015 MrHoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
