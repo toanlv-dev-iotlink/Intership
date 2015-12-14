c36d40af78760b63953ab5a39ded85e39cd10b42
khoabkdn <mrhoa@MrHoas-Mac.local>
2015-12-13 21:19:51 -0800

    lan 4

--- PhamKhanhHoa/HoaBT2/HoaBT2/CDCar.m
+++ PhamKhanhHoa/HoaBT2/HoaBT2/CDCar.m
@@ -1,15 +1,17 @@
 //
-//  ViewController.h
+//  CDCar.m
 //  HoaBT2
 //
 //  Created by MrHoa on 12/13/15.
 //  Copyright (c) 2015 MrHoa. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "CDCar.h"
 
-@interface ViewController : UIViewController
 
+@implementation CDCar
+
+@dynamic name;
+@dynamic stt;
 
 @end
-
---
+++ PhamKhanhHoa/HoaBT2/HoaBT2/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  HoaBT2
+//
+//  Created by MrHoa on 12/13/15.
+//  Copyright (c) 2015 MrHoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
