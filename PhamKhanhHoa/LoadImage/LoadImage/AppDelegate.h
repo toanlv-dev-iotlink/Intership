754d44d6af9790f0c0c82fc1f8d10600fb3551b7
khoabkdn <mrhoa@MrHoas-Mac.local>
2015-12-15 00:24:30 -0800

    Initial Commit

---
+++ PhamKhanhHoa/LoadImage/LoadImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoadImage
+//
+//  Created by MrHoa on 12/15/15.
+//  Copyright (c) 2015 MrHoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhamKhanhHoa/LoadImage/LoadImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoadImage
+//
+//  Created by MrHoa on 12/15/15.
+//  Copyright (c) 2015 MrHoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
