ddaf8d41a56c0f2159c4522d411e151d77cf7f8e
vantoan2509 <vantoan2509@gmail.com>
2015-12-22 17:50:07 +0700

    avoidgame

---
+++ LeVanToan/Assignment2-AvoidGame/Asn2/GameOverViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameOverViewController.h
+//  Asn2
+//
+//  Created by Z on 12/22/15.
+//  Copyright (c) 2015 Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameOverViewController : UIViewController
+
+@end
---
+++ LeVanToan/Assignment2-AvoidGame/Asn2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Asn2
+//
+//  Created by Z on 12/22/15.
+//  Copyright (c) 2015 Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
